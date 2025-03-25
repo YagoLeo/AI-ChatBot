64ea8e63f96a541b8bd40ca09308061295188244
CC <faaa@live.com>
2025-03-25 17:23:26 +0800

    update first page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Importing CSS files
+import './08.css';
+import './24.css';
+import './32.css';
+import './47.css';
+import './69.css';
+import './82.css';
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
